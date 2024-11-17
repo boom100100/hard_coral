e832594269cde3ae3146db3f237860c1b319fe53
bernadette <bsd231@nyu.edu>
2024-11-17 13:02:38 -0500

    added prelim iteration: reading text

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,14 @@
 console.log("Extension has run.");
+
+const tags = document.querySelectorAll("*");
+const allContent = tags[0].innerText;
+
+const cleanContent = (content) => {
+  return content;
+}
+
+const cleanedContent = cleanContent(allContent);
+
+var msg = new SpeechSynthesisUtterance();
+msg.text = cleanedContent;
+window.speechSynthesis.speak(msg);
