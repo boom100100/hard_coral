b9d9278a7be1ae6735796f7a5ca9108c8d92e20f
bernadette <bsd231@nyu.edu>
2024-11-22 17:24:49 -0500

    save point - saving words to rhythm, removed pitch

--- scripts/voice.js
+++ scripts/voice.js
@@ -1,8 +1,12 @@
+let bpm;
+const setBpm = (newBpm) => {
+  bpm = newBpm;
+};
 
 //  https://github.com/mdn/dom-examples/blob/main/web-speech-api/speak-easy-synthesis/script.js
 // then, create tool for generating music
 // figure out syncing voice to music like a proper song
-const voiceClickEventListener =  (e) => {
+const voiceClickEventListener = (e) => {
   // Audio play doesn't always work because the queue gets stuck sometimes.
   // Cancel it until it stops (one cancel per queued tts reading).
   while (window.speechSynthesis.speaking) {
@@ -17,6 +21,62 @@ const voiceClickEventListener =  (e) => {
   }
 
   const cleanedContent = cleanContent(allContent);
+  const msg = new SpeechSynthesisUtterance(cleanedContent);
+  window.speechSynthesis.speak(msg);
+  window.speechSynthesis.pause();
+  // window.speechSynthesis.resume();
+  // window.speechSynthesis.pause();
+  // window.speechSynthesis.resume();
+
+
+
+  const beatToTimeInMilliseconds = (startingBeat, bpm) => {
+    // startTime = 60 / bpm * startingBeat
+    // convert to seconds, find start time, convert to milliseconds
+    return 60 / bpm * startingBeat * 1000;
+    // if 60 bpm
+    // beat 1 = 1 second
+    // 60 / 60 * 1 = 1
+    // beat 2 = 2 seconds
+    // 60 / 60 * 2 = 2
+
+
+    // if 30 bpm
+    // beat 1 = 2 seconds
+    // 60 / 30 * 1 = 2
+    // beat 2 = 4 seconds
+    // 60 / 30 * 2 = 4
+    
+    // if 120 bpm
+    // beat 1 = .5 seconds
+    // 60 / 120 * 1 = .5
+    // beat 2 = 1 seconds
+    // 60 / 120 * 2 = 1
+  };
+
+  let j = 0;
+  const whenToSpeakBeats = [0,3,4,5,6,8,10];
+  const howLongToSpeakBeats = [1,1,1,1,1,1,1];
+  while (window.speechSynthesis.pending && j < whenToSpeakBeats.length) {
+    const startTime = whenToSpeakBeats[j];
+    const duration = howLongToSpeakBeats[j];
+    setTimeout(
+      () => {
+        window.speechSynthesis.resume();
+      },
+      beatToTimeInMilliseconds(startTime, bpm)
+    );
+
+    setTimeout(
+      () => {
+        window.speechSynthesis.pause();
+      },
+      beatToTimeInMilliseconds(startTime + duration, bpm)
+    );
+    j++;
+  }
+    
+
 
   // // individual words
   // const words = cleanedContent.split(" ");
@@ -25,45 +85,48 @@ const voiceClickEventListener =  (e) => {
   // var i = 0;
   // for (let word of words) {
   //   const index = i % 4;
-    
+
   //   const msg = new SpeechSynthesisUtterance(word);
   //   msg.pitch = pitches[index];
   //   msg.rate = rates[index];
-  //   window.speechSynthesis.speak(msg);
-    
+  //   // window.speechSynthesis.speak(msg);
+  //   // window.speechSynthesis.pause();
+
+
+
+
+
+  //   // schedule when to speak
+  //   const elapsedTime = 0;
+  //   const duration = 10; // seconds, can be fractions/irrational numbers
+  //   // 10 seconds = 10 beats
+  //   // const rate = 60 / bpm;
+  //   let j = 0;
+  //   const whenToSpeakBeats = [0,3,4,5,6,8,10];
+  //   while (elapsedTime < duration) {
+  //     setTimeout(
+  //       () => {
+  //         // can pause instead
+  //         // window.speechSynthesis.pause();
+  //         // schedule speech start time, probably best for individual words
+  //         // this places speech in the queue, it can't actually control when the speech will start
+  //         // but cancelling the current running utterance will let this one run now
+  //         window.speechSynthesis.pause();
+  //   window.speechSynthesis.resume();
+
+  //         window.speechSynthesis.cancel();
+  //         window.speechSynthesis.speak(msg);
+  //       },
+  //       startTimeOfBeatMilliseconds(whenToSpeakBeats[j], bpm)
+  //     );
+  //     elapsedTime++;
+  //     j++;
+  //   }
   //   i++;
   // }
-  
-  // word buckets
-  const words = cleanedContent.split(" ");
-  var pitches = [0, 2, 1, 2, 1, 2];
-  var rates = [1, .5, 1.25, .5, 1, 2];
-  var i = 0;
-  const wordBuckets = (() => {
-    const buckets = [];
-    let ks = [1,2,1,3];
-    var j = 0;
-    while (words.length > 0) {
-      const index = j % ks.length;
-      const bucket = words.splice(0, ks[index]);
-      buckets.push(bucket);
-      j++;
-    }
-    return buckets;
-  })();
-
-  for (let bucket of wordBuckets) {
-    const index = i % pitches.length;
-    // do sm
-    const msg = new SpeechSynthesisUtterance(bucket.join(" "));
-    msg.pitch = pitches[index];
-    msg.rate = rates[index];
-    window.speechSynthesis.speak(msg);
-    i++;
-  }
-
 }
 
 export {
   voiceClickEventListener,
+  setBpm,
 }
