a49f279235bbf9291f8e90507a75a9a9d9475427
bernadette <bsd231@nyu.edu>
2024-11-22 18:24:09 -0500

    added word bag approach

--- scripts/voice.js
+++ scripts/voice.js
@@ -8,7 +8,7 @@ const setBpm = (newBpm) => {
 // figure out syncing voice to music like a proper song
 const voiceClickEventListener = (e) => {
   // Audio play doesn't always work because the queue gets stuck sometimes.
-  // Cancel it until it stops (one cancel per queued tts reading).
+  // Cancel it until it stops (one cancel per queued tts utterance).
   while (window.speechSynthesis.speaking) {
     window.speechSynthesis.cancel();
   }
@@ -23,107 +23,70 @@ const voiceClickEventListener = (e) => {
   const cleanedContent = cleanContent(allContent);
   const msg = new SpeechSynthesisUtterance(cleanedContent);
   window.speechSynthesis.speak(msg);
-  window.speechSynthesis.pause();
-  // window.speechSynthesis.resume();
-  // window.speechSynthesis.pause();
-  // window.speechSynthesis.resume();
 
-
-
+  // TODO: move fcn declaration to root of file
   const beatToTimeInMilliseconds = (startingBeat, bpm) => {
     // startTime = 60 / bpm * startingBeat
     // convert to seconds, find start time, convert to milliseconds
     return 60 / bpm * startingBeat * 1000;
-    // if 60 bpm
-    // beat 1 = 1 second
-    // 60 / 60 * 1 = 1
-    // beat 2 = 2 seconds
-    // 60 / 60 * 2 = 2
+    // beats equation logic
+      // if 60 bpm
+        // beat 1 = 1 second
+        // 60 / 60 * 1 = 1
+        // beat 2 = 2 seconds
+        // 60 / 60 * 2 = 2
 
 
-    // if 30 bpm
-    // beat 1 = 2 seconds
-    // 60 / 30 * 1 = 2
-    // beat 2 = 4 seconds
-    // 60 / 30 * 2 = 4
-    
-    // if 120 bpm
-    // beat 1 = .5 seconds
-    // 60 / 120 * 1 = .5
-    // beat 2 = 1 seconds
-    // 60 / 120 * 2 = 1
+      // if 30 bpm
+        // beat 1 = 2 seconds
+        // 60 / 30 * 1 = 2
+        // beat 2 = 4 seconds
+        // 60 / 30 * 2 = 4
+      
+      // if 120 bpm
+        // beat 1 = .5 seconds
+        // 60 / 120 * 1 = .5
+        // beat 2 = 1 seconds
+        // 60 / 120 * 2 = 1
   };
 
-  let j = 0;
-  const whenToSpeakBeats = [0,3,4,5,6,8,10];
+  let k = 0;
+  let l = 0;
+  // some random numbers. timing for this will be way off
+  // because api offers unpredictable end times for speech
+  // TODO: is pitch variable during pause? No.
+  // is rate variable?
+  const whenToSpeakBeats = [0,0, 1,1];
+  const pitches = [.5, 2];
+  const rates = [.3, 1];
+  // const whenToSpeakBeats = [0,.5,1,1.5,4,5,6,7];
+  // const whenToSpeakBeats = [1,0,3,1,0,0,0];
+  // const whenToSpeakBeats = [0,.5,.5,.5,.5,.5,.5];
   const howLongToSpeakBeats = [1,1,1,1,1,1,1];
-  while (window.speechSynthesis.pending && j < whenToSpeakBeats.length) {
-    const startTime = whenToSpeakBeats[j];
-    const duration = howLongToSpeakBeats[j];
+
+  const callback = (j) => {
+    const jIndex = j % whenToSpeakBeats.length;
+    const kIndex = k % pitches.length;
+    const lIndex = l % rates.length;
+    // const duration = howLongToSpeakBeats[index];
+    const startTime = whenToSpeakBeats[jIndex];
+    msg.pitch = pitches[kIndex];
+    msg.rate = rates[lIndex];
     setTimeout(
       () => {
         window.speechSynthesis.resume();
       },
       beatToTimeInMilliseconds(startTime, bpm)
     );
-
-    setTimeout(
-      () => {
-        window.speechSynthesis.pause();
-      },
-      beatToTimeInMilliseconds(startTime + duration, bpm)
-    );
+  };
+  
+  let j = 0;
+  // TODO: note that this approach means it's not possible to split a word
+  msg.onboundary = (e) => {
+    window.speechSynthesis.pause();
+    callback(j);
     j++;
-  }
-    
-
-
-  // // individual words
-  // const words = cleanedContent.split(" ");
-  // var pitches = [0, 2, 1, 1];
-  // let rates = [1, .5, 1.5, .5];
-  // var i = 0;
-  // for (let word of words) {
-  //   const index = i % 4;
-
-  //   const msg = new SpeechSynthesisUtterance(word);
-  //   msg.pitch = pitches[index];
-  //   msg.rate = rates[index];
-  //   // window.speechSynthesis.speak(msg);
-  //   // window.speechSynthesis.pause();
-
-
-
-
-
-  //   // schedule when to speak
-  //   const elapsedTime = 0;
-  //   const duration = 10; // seconds, can be fractions/irrational numbers
-  //   // 10 seconds = 10 beats
-  //   // const rate = 60 / bpm;
-  //   let j = 0;
-  //   const whenToSpeakBeats = [0,3,4,5,6,8,10];
-  //   while (elapsedTime < duration) {
-  //     setTimeout(
-  //       () => {
-  //         // can pause instead
-  //         // window.speechSynthesis.pause();
-  //         // schedule speech start time, probably best for individual words
-  //         // this places speech in the queue, it can't actually control when the speech will start
-  //         // but cancelling the current running utterance will let this one run now
-  //         window.speechSynthesis.pause();
-  //   window.speechSynthesis.resume();
-
-  //         window.speechSynthesis.cancel();
-  //         window.speechSynthesis.speak(msg);
-  //       },
-  //       startTimeOfBeatMilliseconds(whenToSpeakBeats[j], bpm)
-  //     );
-  //     elapsedTime++;
-  //     j++;
-  //   }
-  //   i++;
-  // }
+  };
 }
 
 export {
