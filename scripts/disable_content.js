68c5f256eb6e6faf232bbb9778e9bcfac0f2df8f
bernadette <bsd231@nyu.edu>
2024-11-23 15:50:01 -0500

    updated disable

--- scripts/disable_content.js
+++ scripts/disable_content.js
@@ -1,17 +1,22 @@
 console.log("Extension has been disabled.");
 
-var removeListeners = (clickEventListener, mouseMoveEventListener) => {
-  document.removeEventListener('mousemove', mouseMoveEventListener);
-  document.body.removeEventListener('click', clickEventListener, true);
-};
-
 if ('speechSynthesis' in window) {
   console.log('is supported');
 
   (async () => {
-    const src = chrome.runtime.getURL("scripts/listeners.js");
-    const listeners = await import(src);
-    const {clickEventListener, mouseMoveEventListener} = listeners;
-    removeListeners(clickEventListener, mouseMoveEventListener);
+    const srcMusic = chrome.runtime.getURL("scripts/music.js");
+    const srcSingingVoice = chrome.runtime.getURL("scripts/singing_voice.js");
+
+    const srcMouseMovement = chrome.runtime.getURL("scripts/mouse_movement.js");
+
+    const music = await import(srcMusic);
+    const singingVoice = await import(srcSingingVoice);
+    const mouseMovement = await import(srcMouseMovement);
+    const { mouseMoveEventListener } = mouseMovement;
+    const { musicClickEventListener } = music;
+    const { voiceClickEventListener } = singingVoice;
+    document.removeEventListener('mousemove', mouseMoveEventListener);
+    document.body.removeEventListener('click', musicClickEventListener, true);
+    document.body.removeEventListener('click', voiceClickEventListener, true);
   })();
 }
