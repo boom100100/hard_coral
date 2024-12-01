0d4585d7673322b99892b59e1f7789f0a1ca95de
bernadette <bsd231@nyu.edu>
2024-12-01 17:28:55 -0500

    folded vetted voice names

--- scripts/singing_voice.js
+++ scripts/singing_voice.js
@@ -23,15 +23,11 @@ var reset = () => {
     clearInterval(id);
   }
   // Audio play doesn't always work because the queue gets stuck sometimes.
-  // Cancel currently playing speechSynthesis until it stops speaking
-  // (one cancel per queued tts utterance).
+  // Cancel currently playing speechSynthesis until it stops speaking.
   while (window.speechSynthesis.speaking) {
     window.speechSynthesis.cancel();
   }
 };
-//  https://github.com/mdn/dom-examples/blob/main/web-speech-api/speak-easy-synthesis/script.js
-// then, create tool for generating music
-// figure out syncing voice to music like a proper song
 const voiceClickEventListener = (e) => {
   // prevent overlapping voices
   reset();
@@ -121,62 +117,63 @@ const voiceClickEventListener = (e) => {
       const kIndex = k % pitches.length;
       const lIndex = l % rates.length;
   
-      // const voice = voicesByName["Aaron"]; // 
-      // const voice = voicesByName["Daniel (English (United Kingdom))"]; // 
-      // const voice = voicesByName["Eddy (English (United States))"]; // 
-      // const voice = voicesByName["Ellen"]; // 
-      // const voice = voicesByName["Fred"]; // 
-      // const voice = voicesByName["Gordon"]; // 
-      // const voice = voicesByName["Grandma (English (United Kingdom))"]; // 
-      // const voice = voicesByName["Grandma (English (United States))"]; // 
-      // const voice = voicesByName["Grandpa (English (United Kingdom))"]; // 
-      // const voice = voicesByName["Grandpa (English (United States))"]; // 
-      // const voice = voicesByName["Joana"]; // 
-      // const voice = voicesByName["Junior"]; // 
-      // const voice = voicesByName["Kanya"]; // 
-      // const voice = voicesByName["Li-Mu"]; // 
-      // const voice = voicesByName["Luciana"]; // 
-      // const voice = voicesByName["Marie"]; // 
-      // const voice = voicesByName["Martha"]; // 
-      // const voice = voicesByName["Milena"]; // 
-      // const voice = voicesByName["Moira"]; // 
-      // const voice = voicesByName["Ralph"]; // 
-      // const voice = voicesByName["Reed (English (United Kingdom))"]; // 
-      // const voice = voicesByName["Reed (English (United States))"]; // 
-      // const voice = voicesByName["Rishi"]; // liked, is deep, can vary  pitch
-      // const voice = voicesByName["Rocko (English (United Kingdom))"]; // 
-      const voice = voicesByName["Rocko (English (United States))"]; // 
-      // const voice = voicesByName["Satu"]; // 
-      // const voice = voicesByName["Shelley (English (United Kingdom))"]; // 
-      // const voice = voicesByName["Shelley (English (United States))"]; // 
-      // const voice = voicesByName["Sinji"]; // 
-      // const voice = voicesByName["Superstar"]; // 
-      // const voice = voicesByName["Tessa"]; // 
-      // const voice = voicesByName["Xander"]; // 
-      // const voice = voicesByName["Yelda"]; // 
-      // const voice = voicesByName["Zosia"]; // 
-      // const voice = voicesByName["Zuzana"]; //
-      
-      // can the pitch for the voice vary?
-      // const voice = voicesByName["Albert"]; // y, it can vary
-      // const voice = voicesByName["Bells"]; // n
-      // const voice = voicesByName["Boing"]; // y
-      // const voice = voicesByName["Cellos"]; // y
-      // const voice = voicesByName["Jester"]; // n
-      // const voice = voicesByName["Kyoko"]; // y
-      // const voice = voicesByName["O-Ren"]; // y
-      // const voice = voicesByName["Organ"]; // y
-      // Trinoids sounds horrible
-      // Albert sounds horrible over drums
-      // Boing sounds horrible over drums
-      // Organ sounds horrible over drums
-      // Whisper sounds the best
-      // Wobble is really interesting, I like it.
-      // Zarvox sounds robotic and slightly bouncy, but works well.
-      // const voice = voicesByName["Trinoids"]; // y
-      // const voice = voicesByName["Whisper"]; // n
-      // const voice = voicesByName["Wobble"]; // not sure, seems like no
-      // const voice = voicesByName["Zarvox"]; // y
+      // some potentially useful voice names:
+        // const voice = voicesByName["Aaron"]; // 
+        // const voice = voicesByName["Daniel (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Eddy (English (United States))"]; // 
+        // const voice = voicesByName["Ellen"]; // 
+        // const voice = voicesByName["Fred"]; // 
+        // const voice = voicesByName["Gordon"]; // 
+        // const voice = voicesByName["Grandma (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Grandma (English (United States))"]; // 
+        // const voice = voicesByName["Grandpa (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Grandpa (English (United States))"]; // 
+        // const voice = voicesByName["Joana"]; // 
+        // const voice = voicesByName["Junior"]; // 
+        // const voice = voicesByName["Kanya"]; // 
+        // const voice = voicesByName["Li-Mu"]; // 
+        // const voice = voicesByName["Luciana"]; // 
+        // const voice = voicesByName["Marie"]; // 
+        // const voice = voicesByName["Martha"]; // 
+        // const voice = voicesByName["Milena"]; // 
+        // const voice = voicesByName["Moira"]; // 
+        // const voice = voicesByName["Ralph"]; // 
+        // const voice = voicesByName["Reed (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Reed (English (United States))"]; // 
+        // const voice = voicesByName["Rishi"]; // liked, is deep, can vary  pitch
+        // const voice = voicesByName["Rocko (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Satu"]; // 
+        // const voice = voicesByName["Shelley (English (United Kingdom))"]; // 
+        // const voice = voicesByName["Shelley (English (United States))"]; // 
+        // const voice = voicesByName["Sinji"]; // 
+        // const voice = voicesByName["Superstar"]; // 
+        // const voice = voicesByName["Tessa"]; // 
+        // const voice = voicesByName["Xander"]; // 
+        // const voice = voicesByName["Yelda"]; // 
+        // const voice = voicesByName["Zosia"]; // 
+        // const voice = voicesByName["Zuzana"]; //
+        
+        // can the pitch for the voice vary?
+        // const voice = voicesByName["Albert"]; // y, it can vary
+        // const voice = voicesByName["Bells"]; // n
+        // const voice = voicesByName["Boing"]; // y
+        // const voice = voicesByName["Cellos"]; // y
+        // const voice = voicesByName["Jester"]; // n
+        // const voice = voicesByName["Kyoko"]; // y
+        // const voice = voicesByName["O-Ren"]; // y
+        // const voice = voicesByName["Organ"]; // y
+        // Trinoids sounds horrible
+        // Albert sounds horrible over drums
+        // Boing sounds horrible over drums
+        // Organ sounds horrible over drums
+        // Whisper sounds the best
+        // Wobble is really interesting, I like it.
+        // Zarvox sounds robotic and slightly bouncy, but works well.
+        // const voice = voicesByName["Trinoids"]; // y
+        // const voice = voicesByName["Whisper"]; // n
+        // const voice = voicesByName["Wobble"]; // not sure, seems like no
+        // const voice = voicesByName["Zarvox"]; // y
+      const voice = voicesByName["Rocko (English (United States))"];
       msg.voice = voice;
       const isShort = msg.text.length <= 3;
       const isLong = msg.text.length > 10;
