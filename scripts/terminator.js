782d78ca1051b549343d910bdbe37c52092b3f92
bernadette <bsd231@nyu.edu>
2024-12-06 17:05:10 -0500

    extracted termination functionality

---
+++ scripts/terminator.js
@@ -0,0 +1,58 @@
+const terminate = async () => {
+
+    const srcToggleInteraction = chrome.runtime.getURL("scripts/toggle_interaction.js");
+    const toggleInteraction = await import(srcToggleInteraction);
+    const { enableInteraction } = toggleInteraction;
+
+    enableInteraction();
+
+    const srcMusic = chrome.runtime.getURL("scripts/music.js");
+    const srcSingingVoice = chrome.runtime.getURL("scripts/singing_voice.js");
+    const srcMouseMovement = chrome.runtime.getURL("scripts/mouse_movement.js");
+    const srcSelectors = chrome.runtime.getURL("scripts/selectors.js");
+
+    const music = await import(srcMusic);
+    const singingVoice = await import(srcSingingVoice);
+    const mouseMovement = await import(srcMouseMovement);
+    const selectors = await import(srcSelectors);
+
+    const { mouseMoveEventListener, mouseLeaveEventListener } = mouseMovement;
+    const { musicClickEventListener, reset: resetMusic } = music;
+    const { voiceClickEventListener, reset: resetSinging } = singingVoice;
+    const { remove: removeSettings } = selectors;
+
+    document.removeEventListener('mousemove', mouseMoveEventListener);
+    document.removeEventListener('mouseleave', mouseLeaveEventListener);
+    document.body.removeEventListener('click', musicClickEventListener, true);
+    document.body.removeEventListener('click', voiceClickEventListener, true);
+    
+    resetMusic();
+    resetSinging();
+    removeSettings();
+  }
+
+  const setLocationChangeMonitor = () => {
+    // TODO: maybe one day polling won't be necessary https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+    // Must work for SPAs as well.
+    var oldHref = window.location.href;
+    const id = setInterval(
+      async () => {
+        if (oldHref === window.location.href) {
+          return;
+        }
+
+        const srcTerminator = chrome.runtime.getURL("scripts/terminator.js");
+        const terminator = await import(srcTerminator);
+        const { terminate } = terminator;
+
+        terminate();
+        clearInterval(id);
+      },
+      200
+    );
+  }
+
+  export {
+    setLocationChangeMonitor,
+    terminate,
+  }
