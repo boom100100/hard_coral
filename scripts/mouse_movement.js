d3986deb9647b317dd8a6df705c72fa72b218c9e
bernadette <bsd231@nyu.edu>
2024-12-10 08:56:31 -0500

    covered leave trigger edge case

--- scripts/mouse_movement.js
+++ scripts/mouse_movement.js
@@ -3,6 +3,7 @@ const previousElementProps = {
   previousStyleCssText: undefined,
 };
 
+let hadRecentLeaveEvent = false;
 let currentElement;
 let getShouldExecute;
 let setGetShouldExecute = (newGetShouldExecute) => {
@@ -17,7 +18,10 @@ const mouseMoveEventListener = (e) => {
   // saves element with target text
   currentElement = document.elementFromPoint(e.clientX, e.clientY);
   if (
-    previousElement === currentElement // hovering over the same element
+    (
+      previousElement === currentElement // hovering over the same element
+      && !hadRecentLeaveEvent // edge case: mouse leaving screen might be followed by re-enter ay same element
+    )
     || (getShouldExecute && !getShouldExecute()(currentElement)) // when settings, body, or html element
   ) {
     return;
@@ -39,20 +43,35 @@ const mouseMoveEventListener = (e) => {
   // highlight currentElement
   currentElement.style.background = "#000000";
   currentElement.style.color = "#eeeeee";
+  hadRecentLeaveEvent = false;
 }
 
 const mouseLeaveEventListener = (e) => {
-  // const { previousStyleCssText, previousElement } = previousElementProps;
+  const { previousStyleCssText, previousElement } = previousElementProps;
   
-  // if (
-  //   (getShouldExecute && !getShouldExecute()(currentElement)) // when settings, body, or html element
-  //   // getShouldExecute && getShouldExecute()(currentElement) // note: this should probably match mouseMoveEventListener's approach
-  // ) {
-  //   return;
-  // }
+  if (
+    (getShouldExecute && !getShouldExecute()(currentElement)) // when settings, body, or html element
+  ) {
+    return;
+  }
 
-  // currentElement.style.cssText = previousStyleCssText;
-  // previousElementProps.previousStyleCssText = undefined;
+  // styling
+  const prevOrCurrentElement = (previousElement ?? currentElement);
+  // style.cssText reverts more smoothly than style, for some reason.
+  // style is probably just not writeable.
+  const prevOrCurrentStyleCssText = (previousStyleCssText ?? currentElement.style.cssText);
+
+  // set previous element back how it was
+  prevOrCurrentElement.style.cssText = prevOrCurrentStyleCssText;
+  
+  // looking forward: currentElement (before its change) is the future previousElement
+  previousElementProps.previousElement = currentElement;
+  previousElementProps.previousStyleCssText = currentElement.style.cssText;
+  
+  // highlight currentElement
+  currentElement.style.cssText = prevOrCurrentStyleCssText;
+
+  hadRecentLeaveEvent = true;
 }
 
 export {
